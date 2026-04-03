05d6f16b77c148805629588dc74aac1601392c12
AndreaNOdell <anodell@ucdavis.edu>
2026-04-03 10:06:06 -0700

    Add code to create transient dynamic figures

--- code/make_figures.R
+++ code/make_figures.R
@@ -4,6 +4,7 @@ library(latex2exp)
 library(PNWColors)
 library(ggpubr)
 library(tidyverse)
+library(patchwork)
 load("cleaned_data/lingcod_parms.Rdata")
 load("cleaned_data/rockfish_parms.Rdata")
 
@@ -64,14 +65,15 @@ prey_selectivity_df = as.data.frame(cbind(
   mutate(selectivity = fct_relevel(selectivity, c("None", "Generalist", "Intermediate", "Specialist"))) %>% 
   mutate(outcome = fct_relevel(outcome, c("Spawning Biomass", "Stability", "Age Structure")))
 
-jpeg("plots/final_figures/fig3.jpeg", width =170, height = 110, res = 400, units = "mm")
+jpeg("plots/final_figures/fig3.tiff", width =18, height = 11, res = 300, units = "cm")
 tag_facet2(prey_selectivity_df %>% 
              ggplot(aes(x = selectivity, y = value*100)) +
              theme_classic() +
              geom_hline(yintercept = 0, linetype = 2, color = "grey44") +
              geom_point(aes(fill = selectivity), size = 6, shape = 21, color = "black") +
              facet_wrap( ~ outcome, strip.position = "bottom", scales = "free_x") +
-             theme(axis.text.x=element_text(angle=45,hjust=1, size = 10),
+             theme(text = element_text(size = 10),
+                   axis.text.x=element_text(angle=45,hjust=1, size = 10),
                    axis.text.y=element_text(size = 10),
                    panel.background = element_rect(fill = "white",
                                                    colour = "white",
@@ -99,13 +101,14 @@ load("results/fishing_scenarios_cont_0724/fishing.Rdata")
 generalist_data = fishing_base_selec
 specialist_data = fishing_extreme_selec
 
-# B = 0.1 (high bycatch)
+# B = 0.1 (RUN ONLY FOR high bycatch scenario) ------------------
 load("results/fishing_scenarios_cont_0724/fishing_base_selec_highbycatch.Rdata")
 load("results/fishing_scenarios_cont_0724/fishing_extreme_selec_highbycatch.Rdata")
 load("results/fishing_scenarios_cont_0724/fishing.Rdata")
 
 generalist_data = fishing_base_selec
 specialist_data = fishing_extreme_selec
+# ----------------------------------------
 
 # extract data 
 generalist_avg = unname(unlist(lapply(generalist_data, `[`, "rockfish_avg")))
@@ -164,12 +167,13 @@ df_point = df %>%
   mutate(fishing_scenario = fct_relevel(fishing_scenario, "No fishing", "0.5 F40", "F40"))
 
 # make plot
-jpeg("plots/final_figures/fig4.jpeg", width =170, height = 110, res = 400, units = "mm")
+jpeg("plots/final_figures/fig5.tiff", width =18, height = 11, res = 300, units = "cm")
 tag_facet2(df %>% 
              mutate("variable" = fct_relevel(variable,
                                              "Spawning Biomass", "Stability", "Age Structure")) %>%
              mutate(prey_selectivity = fct_relevel(prey_selectivity,
-                                                   "Generalist", "Specialist")) %>% 
+                                                   "Generalist", "Specialist")) %>%
+             #filter(prey_selectivity == "Generalist") %>% 
              ggplot(aes(x = f40, y = value, shape = prey_selectivity, linetype = prey_selectivity,
                         group = prey_selectivity)) +
              theme_classic() +
@@ -179,7 +183,8 @@ tag_facet2(df %>%
                                                        fill = fishing_scenario), 
                         size = 5, stroke = 0.5, color = "black") +
              lims(x = c(0,1.5), y = c(0.2, 1.05)) +
-             theme(axis.text.x=element_text(size = 10),
+             theme(text = element_text(size = 10),
+                   axis.text.x=element_text(size = 10),
                    axis.text.y=element_text(hjust=1, size = 10),
                    strip.text = element_text(size = 10),
                    panel.background = element_rect(fill = "white",
@@ -195,7 +200,7 @@ tag_facet2(df %>%
              guides(fill = guide_legend(override.aes = list(shape=21))) +
              geom_hline(yintercept = 1, linetype = 2, color = "grey44") +
              scale_fill_manual(values = fishing_scenarios_pal, labels = c("No Fishing", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)'))) +
-             labs(x  = TeX(r'(Lingcod F/$F_{40}$)'), y = "Value Relative to 'None' Scenario", linetype = "Prey Selectivity", shape = "Prey Selectivity", fill = "Fishing Scenarios", color = "Fishing Scenarios")) 
+             labs(x  = TeX(r'(Lingcod F/$F_{40}$)'), y = "Value Relative to 'No Fishing' Scenario", linetype = "Prey Specialization", shape = "Prey Specialization", fill = "Fishing Scenarios", color = "Fishing Scenarios")) 
 dev.off()
 
 
@@ -260,31 +265,37 @@ SB40_df_specialist = as.data.frame(rbind(nofishing_SB40_specialist, lightfishing
   mutate(selectivity = "Specialist")
 SB40_df = as.data.frame(rbind(SB40_df_generalist,SB40_df_specialist))
 
-jpeg("plots/final_figures/fig5.jpeg", width =170, height = 110, res = 400, units = "mm")
+tiff("plots/final_figures/fig6.tiff", width =8.5, height = 7, res = 400, units = "cm") 
 dodge <- position_dodge(width = 0.7)
 SB40_df %>% 
   mutate(selectivity = fct_relevel(selectivity, 
                                    "Generalist", "Specialist")) %>% 
   mutate(fishing = fct_relevel(fishing, "none", "light", "moderate")) %>% 
   ggplot(aes(x=fishing, y=t_SB40, fill = selectivity)) +  #fill = fishing, color = fishing
-  geom_boxplot(notch = FALSE) +
+  geom_boxplot(notch = FALSE, linewidth = 0.3, outlier.size = 0.3) +
   #ylim(0,100) +
   theme_classic() +
   lims(y = c(5,40)) +
-  labs(x = "Lingcod Fishing Scenario", y = TeX(r'(Years until Yelloweye reaches 40% $SB_0$)'), alpha = "Prey-Selectivity", fill = "Prey Selectivity") + 
-  guides(color = FALSE) + 
-  theme(axis.text.x=element_text(size = 10),
-        axis.text.y=element_text(size = 10),
+  labs(x = "Lingcod Fishing Scenario", y = TeX(r'(Years until Yelloweye reaches 40% $SB_0$)'), fill = "Prey Specialization") + 
+  guides(color = FALSE, alpha = FALSE) + 
+  theme(text = element_text(size = 6),
+        axis.text.x=element_text(size = 6),
+        axis.text.y=element_text(size = 6),
         panel.background = element_rect(fill = "white",
                                         colour = "white",
                                         size = 0.5, linewidth = "solid"),
-        panel.border = element_rect(color = "black", fill = NA, size = 0.8),
-        panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+        panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+        panel.grid.major = element_line(size = 0.25, linetype = 'solid',
                                         colour = "gray90"), 
-        panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "gray90")) +
+        panel.grid.minor = element_line(size = 0.15, linetype = 'solid',
+                                        colour = "gray90"),
+        legend.direction = "horizontal", 
+        legend.position = c(0.5,0.87),
+        legend.box = "horizontal",
+        legend.background = element_rect(color = "black", linewidth = 0.25),
+        legend.margin = margin(0.05, 0.15, 0.05, 0.15, unit = "cm")) +
   scale_fill_manual(values = selectivity_pal) +
-  scale_x_discrete(labels = c("none", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)')))
+  scale_x_discrete(labels = c("No Fishing", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)'))) 
 dev.off()
 
 
@@ -314,7 +325,7 @@ rockfish_ts = as.data.frame(pivot_longer(data = rockfish_ts, cols = 1:3, names_t
   mutate(Fishing_Scenario = fct_relevel(Fishing_Scenario,
                                         "none", "0.5F40", "F40")) 
 
-jpeg("plots/final_figures/rockfish_timeseries_deterministic.jpeg", units="in", width=6, height=4, res = 300)
+
 p1 = ggplot(rockfish_ts[rockfish_ts$Relative_SB <= 1.05,], aes(x = year, y = Relative_SB)) +
   geom_line(aes(col = Fishing_Scenario), size = 2) +
   scale_color_manual(values = fishing_scenarios_pal, labels = c("No Fishing", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)'))) + 
@@ -330,7 +341,6 @@ p1 = ggplot(rockfish_ts[rockfish_ts$Relative_SB <= 1.05,], aes(x = year, y = Rel
                                         colour = "gray90")) +
   lims(y = c(0,1.01)) +
   labs(x = "Years", y = TeX(r'(Yelloweye SB/$SB_{0}$)'), color = "Fishing Scenarios")
-dev.off()
 
 
 # timeseries of fishing mortality
@@ -370,7 +380,104 @@ p2 = f_mort_timeseries %>%
   labs(x = "Years", y = TeX(r'(Lingcod F/$F_{40}$)'), color = "Management-Relevent \n Fishing Scenarios")
 dev.off()
 
-jpeg("plots/final_figures/fig2_alternative.jpeg", width =170, height = 85, res = 400, units = "mm")
+tiff("plots/final_figures/fig2.tiff", width =18, height = 9, res = 300, units = "cm")
 ggarrange(p2,p1, ncol = 2, nrow = 1, common.legend = TRUE, labels = c("a)", "b)"), font.label = list(size = 12))
+dev.off()
+
+
+
+# 5. Assess Transient Dynamics ---------------------
+
+load(file = "results/prey_selectivity/no_prey.RData")
+load(file = "results/prey_selectivity/generalist.RData")
+load(file = "results/prey_selectivity/intermediate.RData")
+load(file = "results/prey_selectivity/specialist.RData")
+
+trans_dat = data.frame(Generalist = colMeans(generalist$rockfish_biomass_ts)/max(colMeans(no_prey$rockfish_biomass_ts)),
+                       Intermediate = colMeans(intermediate$rockfish_biomass_ts)/max(colMeans(no_prey$rockfish_biomass_ts)),
+                       Specialist = colMeans(specialist$rockfish_biomass_ts)/max(colMeans(no_prey$rockfish_biomass_ts)),
+                       None = colMeans(no_prey$rockfish_biomass_ts)/max(colMeans(no_prey$rockfish_biomass_ts))) %>%
+  mutate(timestep = 1:length(colMeans(no_prey$rockfish_biomass_ts))) %>% 
+  pivot_longer(cols = c(1:4), values_to = "relSB", names_to = "Scenario") %>% 
+  mutate(selectivity = as.factor(Scenario),
+         selectivity = fct_relevel(selectivity, c("None", "Generalist", "Intermediate", "Specialist")))
+
+tiff("plots/final_figures/FigureS1.tiff", units="in", width=4.5, height=3, res = 300)
+ggplot(trans_dat, aes(x = timestep, y = relSB)) +
+  geom_line(aes(col = selectivity), linewidth = 1.5) + theme_bw() +
+  labs(y = TeX(r'(Yelloweye SB/$SB_{0}$)'), col = "Prey Specialization") +
+  scale_color_manual(values = full_selectivity_pal) + 
+  theme(text = element_text(size = 8)) +
+  lims(x = c(0,150))
+dev.off()
+
+
+# Age structure
+load("results/deterministic/ts_det_generalist_age.RData")
+
+
+p1 = data.frame(no_fish = ts_det_generalist_age[[1]]$rockfish_age_str_ts[1,]/ts_det_generalist_age[[1]]$rockfish_age_str_ts[1,355],
+                mod_fish = ts_det_generalist_age[[2]]$rockfish_age_str_ts[1,]/ts_det_generalist_age[[1]]$rockfish_age_str_ts[1,355],
+                high_fish = ts_det_generalist_age[[3]]$rockfish_age_str_ts[1,]/ts_det_generalist_age[[1]]$rockfish_age_str_ts[1,355]) %>% 
+  mutate(timestep = 1:length(ts_det_generalist_age[[3]]$rockfish_age_str_ts[1,])) %>% 
+  pivot_longer(c(1:3), names_to = "Fishing", values_to = "AgeStruct") %>% 
+  mutate(fishing_scenario = as.factor(Fishing),
+         fishing_scenario = fct_relevel(fishing_scenario, "no_fish", "mod_fish", "high_fish")) %>% 
+  ggplot(aes(x = timestep, y = AgeStruct)) +
+  geom_line(aes(col = fishing_scenario), linewidth = 1.5) +
+  theme_bw() + 
+  scale_color_manual(values = fishing_scenarios_pal, 
+                     labels = c("No Fishing", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)'))) +
+  labs(y = "Age Structure relative to 'No Fishing' scenario", col = "Fishing Scenario") + 
+  theme(text = element_text(size = 8)) +
+  lims(x = c(0,150))
+
+
+p2 = data.frame(no_fish = ts_det_generalist_age[[1]]$rockfish_biomass_ts[1,]/ts_det_generalist_age[[1]]$rockfish_biomass_ts[1,355],
+                mod_fish = ts_det_generalist_age[[2]]$rockfish_biomass_ts[1,]/ts_det_generalist_age[[1]]$rockfish_biomass_ts[1,355],
+                high_fish = ts_det_generalist_age[[3]]$rockfish_biomass_ts[1,]/ts_det_generalist_age[[1]]$rockfish_biomass_ts[1,355]) %>% 
+  mutate(timestep = 1:length(ts_det_generalist_age[[3]]$rockfish_biomass_ts[1,])) %>% 
+  pivot_longer(c(1:3), names_to = "Fishing", values_to = "SB") %>% 
+  mutate(fishing_scenario = as.factor(Fishing),
+         fishing_scenario = fct_relevel(fishing_scenario, "no_fish", "mod_fish", "high_fish")) %>% 
+  ggplot(aes(x = timestep, y = SB)) +
+  geom_line(aes(col = fishing_scenario), linewidth = 1.5) +
+  theme_bw() + 
+  scale_color_manual(values = fishing_scenarios_pal, 
+                     labels = c("No Fishing", TeX(r'(0.5 $F_{40}$)'), TeX(r'($F_{40}$)'))) +
+  labs(y = "Spawning Biomass relative to 'No Fishing' scenario", col = "Fishing Scenario") + 
+  theme(text = element_text(size = 8)) +
+  lims(x = c(0,150))
+
+tiff("plots/final_figures/FigureS2.tiff", units="in", width=6, height=3, res = 300)
+p2 + p1 + plot_layout(guides = 'collect') + plot_annotation(tag_levels = "a", tag_suffix = ")")
+dev.off()
+
+
+
+# 6. Mortality sensitivity Analysis -----------------------------------
+load("results/deterministic/generalist_baseM.RData")
+load("results/deterministic/specialist_baseM.RData")
+load("results/deterministic/specialist_95M.RData")
+load("results/deterministic/specialist_80M.RData")
+dat_M = data.frame(base = specialist_baseM$rockfish_biomass_ts[1,]/generalist_baseM$rockfish_biomass_ts[1,350],
+                   M95 = specialist_95M$rockfish_biomass_ts[1,]/generalist_baseM$rockfish_biomass_ts[1,350],
+                   M80 = specialist_80M$rockfish_biomass_ts[1,]/generalist_baseM$rockfish_biomass_ts[1,350],
+                   generalist = generalist_baseM$rockfish_biomass_ts[1,]/generalist_baseM$rockfish_biomass_ts[1,350],
+                   timestep = 1:length(specialist_baseM$rockfish_biomass_ts[1,])) %>% 
+  pivot_longer(c(1:4), names_to = "Scenario", values_to = "SB") %>% 
+  mutate(Scenario = as.factor(Scenario),
+         Scenario = fct_relevel(Scenario, "generalist", "base", "M95", "M80"))
+
+tiff("plots/final_figures/FigureS5.tiff", units="in", width=5, height=3, res = 300)
+ggplot(dat_M, aes(x = timestep, y = SB)) +
+  geom_line(aes(col = Scenario), linewidth = 2) + theme_bw() +
+  scale_color_manual(values = c("#a45851", "#b0cbe7", "#537380", "#33454e"), 
+                     labels = c(TeX(r'($M^{Y}$ - Generalist)'), TeX(r'($M^{Y}$ - Specialist)'), 
+                                TeX(r'($0.95M^{Y}$ - Specialist)'), TeX(r'($0.8M^{Y}$ - Specialist)'))) +
+  labs(y = "Spawning Biomass relative to generalist scenario", 
+       col = "Natural Mortality Scenario") +
+  theme(text = element_text(size = 8)) +
+  lims(x = c(0,150))
 dev.off()
 
